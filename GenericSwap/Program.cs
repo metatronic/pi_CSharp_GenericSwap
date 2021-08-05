6b7bb50ccf1ddeaecc6d2fdd0eba39a34fa36f8b
pran saha <iampransaha@gmail.com>
2021-08-05 18:34:56 +0530

    Add project files.

---
+++ GenericSwap/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            Console.WriteLine($"num 1: {num1} num2: {num2}");
+            SwapGeneric<int>.Swap(ref num1, ref num2);
+            Console.WriteLine($"num 1: {num1} num2: {num2}");
+
+
+            double numd1 = 10.1;
+            double numd2 = 20.2;
+            Console.WriteLine($"num 1: {numd1} num2: {numd2}");
+            SwapGeneric<double>.Swap(ref numd1, ref numd2);
+            Console.WriteLine($"num 1: {numd1} num2: {numd2}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ GenericSwap/SwapGeneric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSwap
+{
+    static class SwapGeneric<T>
+    {
+        public static void Swap(ref T num1, ref T num2)
+        {
+            T temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+    }
+}
